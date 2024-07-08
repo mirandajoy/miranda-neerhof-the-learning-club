7f049b05c96a675d7d094d5fb7db181755c02869
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-08 09:54:23 -0300

    add db tables

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,10 +1,12 @@
+import "dotenv/config";
+
 export default {
-  client: 'mysql2',
+  client: "mysql2",
   connection: {
-    host: '127.0.0.1',
-    user: root,
+    host: "127.0.0.1",
+    user: "root",
     password: process.env.PW,
     database: process.env.DB,
-    charset: 'utf8',
+    charset: "utf8",
   },
 };
