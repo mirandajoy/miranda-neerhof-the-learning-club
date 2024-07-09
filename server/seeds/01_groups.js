623682df631396ff0bc452589e7c4d46442571b7
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-09 10:20:31 -0300

    remove server from repo

--- server/seeds/01_groups.js
+++
@@ -1,6 +0,0 @@
-import groupsData from "../seed-data/groups.js";
-
-export async function seed(knex) {
-  await knex("groups").del();
-  await knex("groups").insert(groupsData);
-}
--- server/seeds/02_events.js
+++
@@ -1,6 +0,0 @@
-import eventsData from "../seed-data/events.js";
-
-export async function seed(knex) {
-  await knex("events").del();
-  await knex("events").insert(eventsData);
-}
