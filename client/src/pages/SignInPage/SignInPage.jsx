8d785f84e2f60b7a7d59038dbc03ef28ab093855
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-09 09:49:13 -0300

    add sign in form, add signin post call

--- client/src/pages/SignInPage/SignInPage.jsx
+++ client/src/pages/SignInPage/SignInPage.jsx
@@ -1,10 +1,64 @@
 import "./SignInPage.scss";
+import InputField from "../../components/InputField/InputField";
+import Button from "../../components/Button/Button";
+import { useState } from "react";
+import { loginUser } from "../../utils/api-utils";
+import axios from "axios";
 
 const SignInPage = () => {
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleOnChange = (event) => {
+    const target = event.target.name;
+    const newValues = {
+      ...formValues,
+      [target]: event.target.value,
+    };
+    setFormValues(newValues);
+    return newValues;
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    loginExistingUser();
+  };
+
+  const loginExistingUser = async () => {
+    try {
+      const res = await axios.post(loginUser(), formValues);
+      console.log(res);
+      setFormValues({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <>
-      <h1 className="header header--primary">Sign In</h1>
-    </>
+    <section className="sign-in">
+      <h1 className="header header--primary sign-in__header">Sign In</h1>
+      <form id="signInForm" onSubmit={handleOnSubmit} className="sign-in__form">
+        <InputField
+          name="email"
+          type="text"
+          label="Email"
+          placeholder="Email"
+          value={formValues.email}
+          onChange={handleOnChange}
+        />
+        <InputField
+          name="password"
+          type="password"
+          label="Password"
+          placeholder="Password"
+          value={formValues.password}
+          onChange={handleOnChange}
+        />
+        <Button type="Submit" label="Sign in" />
+      </form>
+    </section>
   );
 };
 
--- client/src/utils/api-utils.js
+++ client/src/utils/api-utils.js
@@ -1,3 +1,4 @@
 const apiURL = import.meta.env.VITE_API_URL;
 
 export const createUser = () => `${apiURL}/users/signup`;
+export const loginUser = () => `${apiURL}/users/signin`;
