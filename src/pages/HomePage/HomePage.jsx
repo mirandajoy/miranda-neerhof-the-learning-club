bd25ecb00f2fc9e0c3c0803e4ee664d543a94906
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-09 15:51:53 -0300

    add conditional home page render based on login state

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,23 @@
 import "./HomePage.scss";
+import ButtonLink from "../../components/ButtonLink/ButtonLink";
 
-const HomePage = () => {
+const HomePage = ({ loggedIn, userProfile }) => {
+  if (!loggedIn) {
+    return (
+      <main className="home">
+        <h1 className="header header--primary home__header">Landing Page</h1>
+        <div className="home__button">
+          <ButtonLink link="/signup" label="Sign Up" />
+        </div>
+      </main>
+    );
+  }
+
   return (
-    <>
-      <h1 className="header header--primary">Home</h1>
-    </>
+    <main className="home">
+      <h1 className="header header--primary home__header">Learning Materials</h1>
+      <h2>Hi, {userProfile}</h2>
+    </main>
   );
 };
 
