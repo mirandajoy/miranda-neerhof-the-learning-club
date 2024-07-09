a07c07e432303d8bde3e78d90c4fd87cbfe2f48c
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-09 15:50:53 -0300

    get profile information after sign in or sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,55 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
-import GroupsPage from './pages/GroupsPage/GroupsPage';
-import HomePage from './pages/HomePage/HomePage';
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
-import SignInPage from './pages/SignInPage/SignInPage';
-import SignUpPage from './pages/SignUpPage/SignUpPage';
+import Header from "./components/Header/Header";
+import GroupsPage from "./pages/GroupsPage/GroupsPage";
+import HomePage from "./pages/HomePage/HomePage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import SignInPage from "./pages/SignInPage/SignInPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
+import { getProfile } from "./utils/api-utils";
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userProfile, setUserProfile] = useState(null);
+
+  const token = sessionStorage.getItem("JWTtoken");
+
+  useEffect(() => {
+    if (!!token) {
+      setLoggedIn(true);
+      getUserProfile();
+      console.log(token);
+    }
+  }, [token]);
+
+  const getUserProfile = async () => {
+    try {
+      const res = await axios.get(getProfile(), {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+      setUserProfile(res.data.name);
+      console.log(res.data.name);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("JWTtoken");
+    setLoggedIn(false);
+    setUserProfile(null);
+  };
+
   return (
     <>
       <BrowserRouter>
+        <Header loggedIn={loggedIn} userProfile={userProfile} handleLogout={handleLogout} />
         <Routes>
-          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/" element={<HomePage loggedIn={loggedIn} userProfile={userProfile} />}></Route>
           <Route path="/groups" element={<GroupsPage />}></Route>
           <Route path="/signup" element={<SignUpPage />}></Route>
           <Route path="/signin" element={<SignInPage />}></Route>
--- src/utils/api-utils.js
+++ src/utils/api-utils.js
@@ -2,3 +2,4 @@ const apiURL = import.meta.env.VITE_API_URL;
 
 export const createUser = () => `${apiURL}/users/signup`;
 export const loginUser = () => `${apiURL}/users/signin`;
+export const getProfile = () => `${apiURL}/profiles`;
