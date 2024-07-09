27e129396ad7edf4c9ababeb418f581396bf36f2
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-09 16:33:18 -0300

    add call to get list of groups

--- src/pages/GroupsPage/GroupsPage.jsx
+++ src/pages/GroupsPage/GroupsPage.jsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import "./GroupsPage.scss";
+import { getGroups } from "../../utils/api-utils";
+import axios from "axios";
 
 const GroupsPage = () => {
+  const getGroupsList = async () => {
+    try {
+      const res = await axios.get(getGroups());
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getGroupsList();
+  }, []);
+
   return (
     <>
       <h1 className="header header--primary">Groups</h1>
--- src/utils/api-utils.js
+++ src/utils/api-utils.js
@@ -3,3 +3,4 @@ const apiURL = import.meta.env.VITE_API_URL;
 export const createUser = () => `${apiURL}/users/signup`;
 export const loginUser = () => `${apiURL}/users/signin`;
 export const getProfile = () => `${apiURL}/profiles`;
+export const getGroups = () => `${apiURL}/groups`;
