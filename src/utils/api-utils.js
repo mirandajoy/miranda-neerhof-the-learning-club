dbb8c0cd0ad9832a8f51ba9f86964ce5403b7f84
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-09 21:11:10 -0300

    add utils for joining a group and showing group events

--- src/utils/api-utils.js
+++ src/utils/api-utils.js
@@ -4,3 +4,5 @@ export const createUser = () => `${apiURL}/users/signup`;
 export const loginUser = () => `${apiURL}/users/signin`;
 export const getProfile = () => `${apiURL}/profiles`;
 export const getGroups = () => `${apiURL}/groups`;
+export const joinGroup = (groupId) => `${apiURL}/groups/${groupId}/members`;
+export const getGroupEvents = (groupId) => `${apiURL}/groups/${groupId}/events`;
