35e2fde3473294ae007ae447171561953019dc57
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-13 09:23:41 -0300

    add api call to update an rsvp

--- src/utils/api-events.js
+++ src/utils/api-events.js
@@ -13,9 +13,25 @@ class events {
     this.route = `${apiURL}/events`;
   }
 
+  async getSingleEvent(eventId) {
+    try {
+      return await axios.get(`${this.route}/${eventId}`, authHeader);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async rsvpEvent(eventId, body) {
     try {
-      return await axios.post(`${this.route}/${eventId}/rsvps`, body,authHeader);
+      return await axios.post(`${this.route}/${eventId}/rsvps`, body, authHeader);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async updateEvent(eventId, rsvpId, body) {
+    try {
+      return await axios.put(`${this.route}/${eventId}/rsvps/${rsvpId}`, body, authHeader);
     } catch (error) {
       console.log(error);
     }
