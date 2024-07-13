1d8fde5961a6443a77ac0a0a240a9cb76ddc3715
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-13 10:05:02 -0300

    add new routes for single event and group pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,23 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
 import GroupsPage from "./pages/GroupsPage/GroupsPage";
 import HomePage from "./pages/HomePage/HomePage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import SignInPage from "./pages/SignInPage/SignInPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
-import profiles from './utils/api-profile';
+import SingleEventPage from "./pages/SingleEventPage/SingleEventPage";
+import SingleGroupPage from "./pages/SingleGroupPage/SingleGroupPage";
 
 const App = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [userProfile, setUserProfile] = useState(null);
-
-  const token = sessionStorage.getItem("JWTtoken");
-
-  useEffect(() => {
-    if (!!token) {
-      setLoggedIn(true);
-      getUserProfile();
-    }
-  }, [token]);
-
-  const getUserProfile = async () => {
-    const res = await profiles.getProfile();
-    setUserProfile(res.data.name);
-  };
-
-  const handleLogout = () => {
-    sessionStorage.removeItem("JWTtoken");
-    setLoggedIn(false);
-    setUserProfile(null);
-  };
-
   return (
     <>
       <BrowserRouter>
-        <Header loggedIn={loggedIn} userProfile={userProfile} handleLogout={handleLogout} />
         <Routes>
-          <Route path="/" element={<HomePage loggedIn={loggedIn} userProfile={userProfile} />}></Route>
+          <Route path="/" element={<HomePage />}></Route>
           <Route path="/groups" element={<GroupsPage />}></Route>
+          <Route path="/groups/:id" element={<SingleGroupPage />}></Route>
+          <Route path="/events/:id" element={<SingleEventPage />}></Route>
           <Route path="/signup" element={<SignUpPage />}></Route>
           <Route path="/signin" element={<SignInPage />}></Route>
           <Route path="/*" element={<NotFoundPage />}></Route>
