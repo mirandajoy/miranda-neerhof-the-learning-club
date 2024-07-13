2c980999a0cd7d89f3d3e3f0e4a5b188bfad1267
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-13 10:26:26 -0300

    add support for buttons linking to page anchors

--- src/components/ButtonLink/ButtonLink.jsx
+++ src/components/ButtonLink/ButtonLink.jsx
@@ -1,10 +1,12 @@
 import "./ButtonLink.scss";
 import { Link } from "react-router-dom";
+import { HashLink } from 'react-router-hash-link';
 
-const ButtonLink = ({link, label, styleType}) => {
+const ButtonLink = ({link, label, styleType, anchor}) => {
   return (
     <>
-      <Link to={link} className={`btn-link btn-link--${styleType}`}>{label}</Link>
+      {anchor && <HashLink to={link} className={`btn-link btn-link--${styleType}`}>{label}</HashLink>}
+      {!anchor && <Link to={link} className={`btn-link btn-link--${styleType}`}>{label}</Link>}
     </>
   );
 };
