e937089bf80ebb8e2e1b4b127711ee66778e7186
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-13 09:47:06 -0300

    update event list to use parsed date details and link to single event page

--- src/components/EventListItem/EventListItem.jsx
+++ src/components/EventListItem/EventListItem.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import events from "../../utils/api-events";
+import parseDateTime from "../../utils/time-parse";
 import Button from "../Button/Button";
 import CheckAnimation from "../CheckAnimation/CheckAnimation";
 import "./EventListItem.scss";
 
 const EventListItem = ({ event }) => {
   const [rsvpStatus, setRsvpStatus] = useState(event.status);
+  const parsedDateTime = parseDateTime(event.time);
   const [animated, setAnimated] = useState(false);
 
   const handleRSVP = async (response) => {
@@ -15,39 +18,24 @@ const EventListItem = ({ event }) => {
     setAnimated(true);
   };
 
-  const handleRSVPUpdate = async (response) => {
-    console.log({ response });
-  };
-
-  const convertedDate = new Date(event.time).toLocaleDateString("en-us", {
-    timezone: "America/Halifax",
-    weekday: "long",
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  });
-  const convertedTime = new Date(event.time).toLocaleString("en-us", {
-    hour: "2-digit",
-    minute: "2-digit",
-    timezone: "America/New_York",
-  });
-
   return (
     <div className="event">
-      <div className="event__date-container">
-        <p className="body body--dark body--small event__month">September</p>
-        <p className="body body--dark body--display event__day">25</p>
-      </div>
-      <div className="event__details-container">
-        <p className="body body--dark">Location: {event.location}</p>
-        <p className="body body--dark">Time: {convertedTime}</p>
-      </div>
-      <div className="event__action-container">
-        {rsvpStatus === "Attending" ? (
-          <div className="event__response-container">
-            <CheckAnimation animate={animated} />
-            <span className="body body--dark event__response">Attending!</span>
+      <Link to={`events/${event.id}`} className="event__content-container">
+        <div className="event__date-container">
+          <p className="body body--dark body--small event__month">{parsedDateTime.month}</p>
+          <p className="body body--dark body--display event__day">{parsedDateTime.day}</p>
+        </div>
+        <div className="event__details-container">
+          <p className="body body--dark">Hosted by: Halifax Group</p>
+          <div>
+            <p className="body body--dark body--small">Meet at {event.location}</p>
+            <p className="body body--dark body--small">{parsedDateTime.fullTime}</p>
           </div>
+        </div>
+      </Link>
+      <div className="event__action-container">
+        {rsvpStatus && rsvpStatus.toLowerCase() === "attending" ? (
+          <CheckAnimation animate={animated} label="Attending!" check="true" />
         ) : (
           <Button label="Attend" styleType="secondary" action={() => handleRSVP("Attending")} />
         )}
