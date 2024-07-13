5ec40a278cd412f60f765b1587e0ce178db868bc
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-13 09:52:29 -0300

    update the check animation to include text and incorportate other icons

--- src/components/CheckAnimation/CheckAnimation.jsx
+++ src/components/CheckAnimation/CheckAnimation.jsx
@@ -1,9 +1,15 @@
 import "./CheckAnimation.scss";
 
-const CheckAnimation = ({animate}) => {
+const CheckAnimation = ({ animate, label, check, icon }) => {
   return (
-    <div className="check">
-      <div className={`check__icon ${animate ? "check__icon--animated" : ""}`}></div>
+    <div className="success-check">
+      {check && (
+        <div className="check">
+          <div className={`check__icon ${animate ? "check__icon--animated" : ""}`}></div>
+        </div>
+      )}
+      {icon && <span className="material-symbols-outlined">{icon}</span>}
+      <span className="body body--dark success-check__text">{label}</span>
     </div>
   );
 };
