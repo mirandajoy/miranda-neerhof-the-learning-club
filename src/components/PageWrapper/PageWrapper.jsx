fb4ecdb43525ecf1d45e861a36de04b8d7a0d591
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-13 09:58:09 -0300

    add back button option to page wrapper

--- src/components/PageWrapper/PageWrapper.jsx
+++ src/components/PageWrapper/PageWrapper.jsx
@@ -1,15 +1,26 @@
 import "./PageWrapper.scss";
+import { Link } from "react-router-dom";
 
-const PageWrapper = ({ preHeader, header, width, children }) => {
+const PageWrapper = ({ preHeader, header, width, children, back }) => {
   return (
     <main className={`page-wrapper page-wrapper--${width}`}>
-      {header && (
-        <div className="page-wrapper__header-container">
-          {preHeader && <h3 className="header header--tertiary">{preHeader}</h3>}
-          <h1 className="header header--primary page-wrapper__header">{header}</h1>
-        </div>
-      )}
-      {children}
+      <div className="page-wrapper__inner-wrapper">
+        {back && (
+          <Link to={back} className="page-wrapper__back-container">
+            <span className="material-symbols-outlined page-wrapper__back-icon">arrow_back</span>
+            <span className="page-wrapper__back-text">
+              Back
+            </span>
+          </Link>
+        )}
+        {header && (
+          <div className="page-wrapper__header-container">
+            {preHeader && <h3 className="header header--tertiary">{preHeader}</h3>}
+            <h1 className="header header--primary page-wrapper__header">{header}</h1>
+          </div>
+        )}
+        {children}
+      </div>
     </main>
   );
 };
