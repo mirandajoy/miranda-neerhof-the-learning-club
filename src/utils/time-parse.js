e508287afee58e0b9a63b69e20ddad831b6e51ef
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-14 19:37:30 -0300

    remove fixed timezones from date parsing

--- src/utils/time-parse.js
+++ src/utils/time-parse.js
@@ -1,6 +1,5 @@
 const parseDateTime = (timestamp) => {
   const fullDate = new Date(timestamp).toLocaleDateString("en-us", {
-    timezone: "America/Halifax",
     weekday: "long",
     year: "numeric",
     month: "long",
@@ -9,19 +8,16 @@ const parseDateTime = (timestamp) => {
 
 
   const day = new Date(timestamp).toLocaleDateString("en-us", {
-    timezone: "America/Halifax",
     day: "numeric",
   });
 
   const month = new Date(timestamp).toLocaleDateString("en-us", {
-    timezone: "America/Halifax",
     month: "long",
   });
 
   const fullTime = new Date(timestamp).toLocaleString("en-us", {
     hour: "2-digit",
     minute: "2-digit",
-    timezone: "America/Halifax",
   });
 
   const convertedDateTime = {
