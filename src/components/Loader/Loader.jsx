ea6b41060e206445f41e6e93cccc40fc0f319cef
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-14 11:31:15 -0300

    add loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import "./Loader.scss";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loader__inner-container"></div>
+    </div>
+  );
+};
+
+export default Loader;
