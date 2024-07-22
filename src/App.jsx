0ae5c12839a70443fa42cf3d64198467228b98a9
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-22 17:46:00 -0300

    add route to create new events

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LoginContextProvider from "./components/LoginContextProvider/LoginContextProvider";
+import CreateEventPage from "./pages/CreateEventPage/CreateEventPage";
 import CreateGroupPage from "./pages/CreateGroupPage/CreateGroupPage";
 import GroupsPage from "./pages/GroupsPage/GroupsPage";
 import HomePage from "./pages/HomePage/HomePage";
@@ -24,6 +25,7 @@ const App = () => {
           <Route path="/groups" element={<GroupsPage />}></Route>
           <Route path="/groups/create" element={<CreateGroupPage />}></Route>
           <Route path="/groups/:id" element={<SingleGroupPage />}></Route>
+          <Route path="/events/create" element={<CreateEventPage />}></Route>
           <Route path="/events/:id" element={<SingleEventPage />}></Route>
           <Route path="/signup" element={<SignUpPage />}></Route>
           <Route path="/signin" element={<SignInPage />}></Route>
