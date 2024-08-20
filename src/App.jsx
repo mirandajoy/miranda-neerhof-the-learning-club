df284eb15877a26c5224197bd708878614cbc3a7
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-08-20 10:03:07 -0300

    hide footer after login

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import LoginContextProvider from "./components/LoginContextProvider/LoginContextProvider";
+import { useLogin } from "./components/LoginContextProvider/LoginContextProvider";
 import CreateEventPage from "./pages/CreateEventPage/CreateEventPage";
 import CreateGroupPage from "./pages/CreateGroupPage/CreateGroupPage";
+import EditEventPage from "./pages/EditEventPage/EditEventPage";
+import EditGroupPage from "./pages/EditGroupPage/EditGroupPage";
 import GroupsPage from "./pages/GroupsPage/GroupsPage";
 import HomePage from "./pages/HomePage/HomePage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
@@ -12,31 +14,30 @@ import SignInPage from "./pages/SignInPage/SignInPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import SingleEventPage from "./pages/SingleEventPage/SingleEventPage";
 import SingleGroupPage from "./pages/SingleGroupPage/SingleGroupPage";
-import EditEventPage from "./pages/EditEventPage/EditEventPage";
-import EditGroupPage from "./pages/EditGroupPage/EditGroupPage";
 
 import "./App.scss";
 
 const App = () => {
+  const loggedIn = useLogin();
+  console.log(loggedIn);
+
   return (
     <BrowserRouter>
-      <LoginContextProvider>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/groups" element={<GroupsPage />}></Route>
-          <Route path="/groups/create" element={<CreateGroupPage />}></Route>
-          <Route path="/groups/edit/:id" element={<EditGroupPage />}></Route>
-          <Route path="/groups/:id" element={<SingleGroupPage />}></Route>
-          <Route path="/events/create" element={<CreateEventPage />}></Route>
-          <Route path="/events/edit/:id" element={<EditEventPage />}></Route>
-          <Route path="/events/:id" element={<SingleEventPage />}></Route>
-          <Route path="/signup" element={<SignUpPage />}></Route>
-          <Route path="/signin" element={<SignInPage />}></Route>
-          <Route path="/*" element={<NotFoundPage />}></Route>
-        </Routes>
-        <Footer />
-      </LoginContextProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/groups" element={<GroupsPage />}></Route>
+        <Route path="/groups/create" element={<CreateGroupPage />}></Route>
+        <Route path="/groups/edit/:id" element={<EditGroupPage />}></Route>
+        <Route path="/groups/:id" element={<SingleGroupPage />}></Route>
+        <Route path="/events/create" element={<CreateEventPage />}></Route>
+        <Route path="/events/edit/:id" element={<EditEventPage />}></Route>
+        <Route path="/events/:id" element={<SingleEventPage />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route path="/signin" element={<SignInPage />}></Route>
+        <Route path="/*" element={<NotFoundPage />}></Route>
+      </Routes>
+      {!loggedIn && <Footer />}
     </BrowserRouter>
   );
 };
